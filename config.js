ada7342c74d2df4778d12c5fcac99ea1344e64a2
linkledger98 <linkledger98@gmail.com>
2026-01-20 05:45:02 +0200

    Fix config.js and remove unicode issues

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 window.APP_CONFIG = {
-  API_BASE_URL: "https://cashloan-backend.onrender.com"
-};
-
-console.log("config.js loaded", window.APP_CONFIG);
+    API_BASE_URL: "https://cashloan-backend.onrender.com"
+    };
+    console.log("config loaded");
